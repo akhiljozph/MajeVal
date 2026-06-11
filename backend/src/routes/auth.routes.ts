1a84f57ea6c4ae82ca3cafa0bceaa34f9e803eb0
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-11 23:16:57 +0530

    MAJV-18 - API implementation for signup.

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,15 +1,28 @@
 import { Router, type Request, type Response } from "express";
+
+import AuthController from "../controllers/auth.controller.ts";
+import { container } from "../di-container.ts";
 import { validate } from "../middlewares/validation.middleware.ts";
 import { signupSchema } from "../schemas/auth.schema.ts";
 
 const authRouter = Router();
 
+const authController = container.get<AuthController>(AuthController);
+
 authRouter.post('/signup',
     validate(signupSchema),
-    (req: Request, res: Response) => {
-        res.status(201).json({
-            "message": "Created"
-        })
+    async (req: Request, res: Response) => {
+        try {
+            // await authController.
+
+            res.status(201).json({
+                "message": "Created"
+            });
+        } catch (error: any) {
+            console.error(error);
+        } finally {
+            console.info('POST /api/v1/signup: Invocation completed');
+        }
     }
 );
 
