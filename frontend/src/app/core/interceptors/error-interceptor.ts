df85bf46546c73281068c1a0226a7e16653c2765
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-20 19:10:13 +0530

    MAJV-22 - Implementation of error interceptor.

--- frontend/src/app/core/interceptors/error-interceptor.ts
+++ frontend/src/app/core/interceptors/error-interceptor.ts
@@ -1,5 +1,44 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe(
+    catchError((error: unknown) => {
+
+      if (error instanceof HttpErrorResponse) {
+
+        const backendMessage = error.error?.message || 'An unexpected server error occurred.';
+
+        switch (error.status) {
+          case 400:
+            alert(`Validation Failed: ${backendMessage}`);
+            break;
+
+          case 401:
+            alert(`Session Expired: ${backendMessage}`);
+            break;
+
+          case 403:
+            alert(`Access Denied: You do not have permission for this action.`);
+            break;
+
+          case 429:
+            alert(`Too many requests: ${backendMessage}`);
+            break;
+
+          case 500:
+            alert(`Server Error: ${backendMessage}`);
+            break;
+
+          default:
+            alert(backendMessage);
+            break;
+        }
+      } else {
+        console.error('An internal frontend engine error occurred:', error);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
