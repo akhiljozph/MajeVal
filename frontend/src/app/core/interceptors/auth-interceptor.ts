7e67894d4aa5c685d1cf7bfdff2be59a1ab98594
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-20 19:16:32 +0530

    MAJV-24 - Implementation of auth interceptor.

--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+import { environment } from '../../../environments/environment.development';
+
+export const IS_PUBLIC_API = new HttpContextToken<boolean>(() => false);
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const baseUrl = environment.baseUrl;
+  const token = 'ss' // localStorage.getItem('token');
+  const isPublic = req.context.get(IS_PUBLIC_API);
+
+  const fullUrl = req.url.startsWith('http') ? req.url : `${baseUrl}${req.url}`;
+
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json'
+  };
+
+  if (token && !isPublic) {
+    console.log('Inside token verification.')
+  }
+
+  let modifiedReq = req.clone({
+    url: fullUrl,
+    setHeaders: headers
+  });
+
+  return next(modifiedReq).pipe(
+    catchError((error) => {
+      return throwError(() => error);
+    })
+  );
 };
