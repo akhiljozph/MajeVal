27b56f683b8835edb98c3d767e29e866f0457004
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-24 23:08:42 +0530

    MAJV-32 - Implemented reactive forms in sign in form.

--- frontend/src/app/components/sign-in/sign-in.ts
+++ frontend/src/app/components/sign-in/sign-in.ts
@@ -1,11 +1,26 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'maj-sign-in',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-in.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './sign-in.scss',
 })
-export class SignIn {}
+export class SignIn {
+  signInForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor() { }
+
+  onSubmit() {
+    console.warn(this.signInForm.value);
+  }
+}
