152fc84032a7f1034188610ce6902629adfffa69
Akhil Joseph <ajpulluvelil@gmail.com>
2026-08-04 14:30:06 +0530

    MAJV-53 - Sign In form implementation and password visibility implementations.

--- frontend/src/app/components/auth/sign-in/sign-in.ts
+++ frontend/src/app/components/auth/sign-in/sign-in.ts
@@ -1,19 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'maj-sign-in',
   imports: [
     RouterLink,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './sign-in.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './sign-in.scss',
 })
 export class SignIn {
+
+  hidePassword = signal(true);
+
   signInForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
@@ -22,6 +34,10 @@ export class SignIn {
   constructor(
     private authService: AuthService,
   ) { }
+
+  togglePasswordVisibility(event: any) {
+    this.hidePassword.set(!this.hidePassword());
+  }
 
   onSubmit() {
     this.authService.accountSignIn(this.signInForm.value).subscribe({
