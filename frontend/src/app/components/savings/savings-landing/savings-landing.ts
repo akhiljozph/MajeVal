9ec8954b77da9bb84f10724ad479e7f910922058
Akhil Joseph <ajpulluvelil@gmail.com>
2026-08-16 00:34:21 +0530

    MAJV-59 - Removed wrong modules.

--- frontend/src/app/components/savings/savings-landing/savings-landing.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'maj-savings-landing',
-  imports: [],
-  templateUrl: './savings-landing.html',
-  styleUrl: './savings-landing.scss',
-})
-export class SavingsLanding {}
