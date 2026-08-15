7d18657fe8a699ade104021cec8e9f186296b148
Akhil Joseph <ajpulluvelil@gmail.com>
2026-08-16 00:01:07 +0530

    MAJV-58 - New modules expenses, and savings.

---
+++ frontend/src/app/components/expenses/expenses-landing/expenses-landing.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maj-expenses-landing',
+  imports: [],
+  templateUrl: './expenses-landing.html',
+  styleUrl: './expenses-landing.scss',
+})
+export class ExpensesLanding {}
